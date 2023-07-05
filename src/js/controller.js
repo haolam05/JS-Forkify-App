bc9172c6e3d656a6e1123f758c011d36badbd394
Hao Lam <tuonghao2001@gmail.com>
2023-07-05 10:51:39 -0700

    fetch ingredients from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    return recipe;
+  } catch (err) {
+    throw err;
+  }
+};
+(async () => {
+  try {
+    const recipe = await showRecipe();
+    console.log(recipe);
+  } catch (err) {
+    alert(err.message);
+  }
+})();
